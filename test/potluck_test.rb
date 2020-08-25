cd08c088a304b2c787e6438e7ed7ceacadb616f3
Sage Gonzales <sagegonzales15@gmail.com>
2020-08-24 19:55:02 -0600

    passes potluck has attributes test

---
+++ test/potluck_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+require './lib/potluck'
+
+class PutluckTest < Minitest::Test
+
+  def test_it_exists
+    potluck = Potluck.new("7-13-18")
+    assert_instance_of Potluck, potluck
+  end
+
+  def test_it_has_attributes
+    potluck = Potluck.new("7-13-18")
+
+    assert_equal "7-13-18", potluck.date
+    assert_equal [], potluck.dishes 
+  end
+end
